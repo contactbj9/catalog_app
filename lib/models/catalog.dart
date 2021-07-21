56975617c9416a0e3d59cb5294ef594bd2dc78a3
contactbj9 <contactbj969@gmail.com>
2021-07-21 13:09:10 +0530

    making product card dynamic

---
+++ lib/models/catalog.dart
@@ -0,0 +1,20 @@
+// class item {
+//   final int id;
+//   final String name;
+//   final String desc;
+//   final num price;
+//   final String image;
+
+//   item({this.id, this.name, this.desc, this.price, this.image});
+// }
+
+// final products = [
+//   item(
+//     id: 1,
+//     name: " i phone 12 pro",
+//     desc: "good",
+//     price: 345,
+//     image:
+//         "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS5D1y9HttB8BUiyr1sOxkxIPNklSelv51ybHVzhY8UgJgPgZvHNTXLaN55BXy2TcsJ0DGD7Dr5&usqp=CAc",
+//   )
+// ];
